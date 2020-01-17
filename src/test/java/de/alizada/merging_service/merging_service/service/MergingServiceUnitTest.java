9603f8496f19190d17cda13ba4b1443f4bb0df81
ismayilalizada <ismail.alizadeh@yandex.com>
2020-01-17 13:22:20 +0100

    Merging | new test case added

--- src/test/java/de/alizada/merging_service/merging_service/service/MergingServiceUnitTest.java
+++ src/test/java/de/alizada/merging_service/merging_service/service/MergingServiceUnitTest.java
@@ -5,7 +5,9 @@ import de.alizada.merging_service.merging_service.repository.MergingRepository;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.invocation.InvocationOnMock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.mockito.stubbing.Answer;
 import org.springframework.web.client.HttpClientErrorException;
 
 import static org.junit.Assert.assertEquals;
@@ -40,6 +42,25 @@ public class MergingServiceUnitTest {
         assertThrows(BadRequestException.class, () -> {
             mergingService.merge(2);
         });
+    }
+
+    @Test
+    public void shouldResponseCorrectDataByRequestsId() {
+        when(mergingRepository.findUserById(1)).thenReturn("User1");
+        when(mergingRepository.findCommentsById(1)).thenAnswer((Answer<String>) invocation -> {
+            Thread.sleep(1000);
+            return "Comments1";
+        });
+
+        when(mergingRepository.findUserById(2)).thenReturn("User2");
+        when(mergingRepository.findCommentsById(2)).thenReturn("Comments2");
+
+        Runnable t1 = () -> assertEquals(mergingService.merge(1), "User1Comments1");
+        Runnable t2 = () -> assertEquals(mergingService.merge(2), "User2Comments2");
+
+        new Thread(t1).run();
+        new Thread(t2).run();
+
     }
 
 }
